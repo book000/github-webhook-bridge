3106de86855c22d28519462c9a2c8a40cd73cccd
Tomachi [ICHIGO] <lego@aol.jp>
2021-04-04 04:49:34 +0900

    init

---
+++ config.sample.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = [
+    // Secret to set to github (STRING)
+    // If not set, specify NULL.
+    // It is strongly recommended that you specify this for security reasons.
+    "WEBHOOK_SECRET" => null,
+
+    // Webhook URL to send after bridging (STRING)
+    // e.g. Slack, Discord etc
+    "WEBHOOK_URL" => "http://localhost:8080",
+
+    // List of repositories that do not bridge (ARRAY)
+    "EXCLUDE_REPOSITORYS" => [
+        "book000/github-webhook-bridge",
+    ],
+
+    // List of bridging repositories (ARRAY)
+    "INCLUDE_REPOSITORYS" => null,
+
+    // Log file directory path (STRING)
+    // If you do not want to output a log, specify NULL
+    // e.g. "LOG_DIR" => "/tmp/github-webhook-bridge/",
+    // Log file is output to {REPOSITORY_NAME}/{DATETIME}.log
+    // By default, it is created in the logs directory.
+    "LOG_DIR" => __DIR__ . "/logs/",
+
+    // Time zone (STRING)
+    // If the time zone is not correct and want to change due to the setting in php.ini etc.
+    // If not set, specify NULL.
+    // List of Supported Timezones: https://www.php.net/manual/en/timezones.php
+    "TIME_ZONE" => "UTC",
+];
---
+++ index.php
@@ -0,0 +1,139 @@
+<?php
+require_once(__DIR__ . "/config.php");
+$LOG_PATH = null;
+
+if ($CONFIG["TIME_ZONE"] != null && date_default_timezone_get() == $CONFIG["TIME_ZONE"]) {
+    date_default_timezone_set($CONFIG["TIME_ZONE"]);
+}
+
+// https://www.php.net/manual/ja/function.getallheaders.php#84262
+if (!function_exists("getallheaders")) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == "HTTP_") {
+                $headers[str_replace(" ", "-", ucwords(strtolower(str_replace("_", " ", substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
+/**
+ * Output text to log
+ *
+ * @param string $str
+ * @return void
+ */
+function writeLog($str)
+{
+    global $_SERVER, $LOG_PATH;
+    $datetime = date("Y-m-d H:i:s");
+
+    echo "[$datetime] $str\n";
+
+    if ($LOG_PATH == null) {
+        return;
+    }
+    file_put_contents($LOG_PATH, "[$datetime] {$_SERVER["REMOTE_ADDR"]} $str\n", FILE_APPEND | LOCK_EX);
+}
+
+
+$body = file_get_contents("php://input");
+$payload = json_decode($body, true);
+if (json_last_error() != JSON_ERROR_NONE) {
+    http_response_code(400);
+    writeLog("Parse JSON failed.");
+    exit;
+}
+
+$owner = isset($payload["repository"]["owner"]["login"]) ?
+    $payload["repository"]["owner"]["login"] :
+    null;
+$repository = isset($payload["repository"]["name"]) ?
+    $payload["repository"]["name"] :
+    null;
+
+if ($CONFIG["LOG_DIR"] != null) {
+    if (!file_exists($CONFIG["LOG_DIR"])) {
+        mkdir($CONFIG["LOG_DIR"], 0777, true);
+    }
+    if (!is_writable($CONFIG["LOG_DIR"]) || !is_dir($CONFIG["LOG_DIR"])) {
+        http_response_code(500);
+        writeLog("You do not have permission to write to the log directory.");
+        exit;
+    }
+
+    if ($owner != null && $repository != null) {
+        $LOG_REPO_DIR = $CONFIG["LOG_DIR"] . "/" . basename($owner) . "/" . basename($repository);
+    } else {
+        $LOG_REPO_DIR = $CONFIG["LOG_DIR"] . "/NULL/";
+    }
+
+    if (!file_exists($LOG_REPO_DIR)) {
+        mkdir($LOG_REPO_DIR, 0777, true);
+    }
+    if (!is_writable($LOG_REPO_DIR) || !is_dir($LOG_REPO_DIR)) {
+        http_response_code(500);
+        writeLog("You do not have permission to write to the log repository directory.");
+        exit;
+    }
+
+    $LOG_PATH = $LOG_REPO_DIR . "/" . date("Ymd") . ".log";
+}
+
+$headers = getallheaders();
+
+// Secret check
+if ($CONFIG["WEBHOOK_SECRET"] != null) {
+    $hmac = hash_hmac("sha1", file_get_contents("php://input"), $CONFIG["WEBHOOK_SECRET"]);
+
+    if (!isset($headers["X-Hub-Signature"])) {
+        http_response_code(400);
+        writeLog("Secret key not found.");
+        exit;
+    }
+    if (isset($headers["X-Hub-Signature"]) && ($headers["X-Hub-Signature"] != "sha1=" . $hmac)) {
+        http_response_code(400);
+        writeLog("Secret key error.");
+        exit;
+    }
+}
+
+$repo_name = $payload["repository"]["full_name"];
+if ($CONFIG["EXCLUDE_REPOSITORYS"] != null && in_array($repo_name, $CONFIG["EXCLUDE_REPOSITORYS"])) {
+    http_response_code(400);
+    writeLog("This repository is excluded.");
+    exit;
+}
+if ($CONFIG["INCLUDE_REPOSITORYS"] != null && !in_array($repo_name, $CONFIG["INCLUDE_REPOSITORYS"])) {
+    http_response_code(400);
+    writeLog("This repository is not a bridged repository.");
+    exit;
+}
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $CONFIG["WEBHOOK_URL"]);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, true);
+$response = curl_exec($curl);
+
+$response_code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+$response_header = substr($response, 0, $header_size);
+$response_body = substr($response, $header_size);
+curl_close($curl);
+
+writeLog("Webhook response code: " . $response_code);
+writeLog("Webhook response header: " . $response_header);
+writeLog("Webhook response body: " . $response_body);
+
+if ($response_code <= 299) {
+    writeLog("Successfully sent.");
+} else {
+    writeLog("Failed sent.");
+}
