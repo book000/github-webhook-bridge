ade8cf7a1a7ad9c024bc7dff3fc6199af60c974a
Tomachi <8929706+book000@users.noreply.github.com>
2023-10-29 04:14:33 +0900

    feat: ping event supported (#7)

--- src/actions/ping.ts
+++ src/actions/ping.ts
@@ -1,8 +1,39 @@
 import { PingEvent } from '@octokit/webhooks-types'
 import { BaseAction } from '.'
+import { createEmbed } from '@/utils'
 
 export class PingAction extends BaseAction<PingEvent> {
   public run(): Promise<void> {
-    throw new Error('Method not implemented.')
+    const { zen, repository, hook, sender, organization } = this.event
+
+    const embed = createEmbed(this.eventName, {
+      title: 'Received a ping event',
+      fields: [
+        {
+          name: 'Zen',
+          value: zen,
+        },
+        {
+          name: 'Hook Type',
+          value: hook.type,
+        },
+        {
+          name: 'Repository',
+          value: repository?.full_name ?? 'N/A',
+        },
+        {
+          name: 'Sender',
+          value: sender?.login ?? 'N/A',
+        },
+        {
+          name: 'Organization',
+          value: organization?.login ?? 'N/A',
+        },
+      ],
+    })
+
+    return this.discord.sendMessage({
+      embeds: [embed],
+    })
   }
 }
