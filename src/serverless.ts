6856e7ae7f8ba8b29dca1d6a13d2403c9c4cf223
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-11-12 23:20:30 +0900

    fix: vercel/node

--- src/serverless.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { getApp } from './main'
-
-export default async (request: FastifyRequest, response: FastifyReply) => {
-  const app = await getApp()
-  await app.ready()
-  app.server.emit('request', request, response)
-}
