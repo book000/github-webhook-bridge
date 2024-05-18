754f3e6cb01e571ebee957b94965c904ecb30e62
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2024-05-19 02:34:46 +0900

    fix: lint

--- src/tests/get-embed.test.ts
+++ src/tests/get-embed.test.ts
@@ -1,8 +1,8 @@
-import { DiscordMessage } from "@book000/node-utils"
-import { getApp } from "../main"
+import { DiscordMessage } from '@book000/node-utils'
+import { getApp } from '../main'
 import fs from 'node:fs'
-import { FastifyInstance } from "fastify"
-import WebhookDefinitions from "@octokit/webhooks-examples"
+import { FastifyInstance } from 'fastify'
+import WebhookDefinitions from '@octokit/webhooks-examples'
 import { BaseAction } from '../actions'
 import * as utils from '../utils'
 
@@ -14,37 +14,47 @@ describe('Get embed', () => {
     await app.ready()
 
     process.env.GITHUB_WEBHOOK_SECRET = '1234567890'
-    process.env.DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1234567890/1234567890'
+    process.env.DISCORD_WEBHOOK_URL =
+      'https://discord.com/api/webhooks/1234567890/1234567890'
 
     jest.spyOn(utils, 'isSignatureValid').mockReturnValue(true)
 
     jest.spyOn(utils, 'getUsersMentions').mockImplementation((userOrTeams) => {
-      return Promise.resolve(userOrTeams.map((userOrTeam) => {
-        return 'login' in userOrTeam ? `@${userOrTeam.login}` : null
-      }).join(' '))
+      return Promise.resolve(
+        userOrTeams
+          .map((userOrTeam) => {
+            return 'login' in userOrTeam ? `@${userOrTeam.login}` : null
+          })
+          .join(' ')
+      )
     })
   })
 
-  it.each(WebhookDefinitions.map(
-    (definition) => [definition.name, definition]
-  ))('should handle %s', async (_, definition) => {
+  it.each(
+    WebhookDefinitions.map((definition) => [definition.name, definition])
+  )('should handle %s', async (_, definition) => {
     for (const [exampleId, example] of definition.examples.entries()) {
-      const mockInstance = jest.spyOn(BaseAction.prototype as unknown as {
-        sendMessage: (key: string, message: DiscordMessage) => Promise<void>
-      }, 'sendMessage').mockImplementationOnce((_: string, message: DiscordMessage) => {
-        const eventName = definition.name
-        const action = 'action' in example ? example.action : ''
-        const path = `data/debug/${eventName}/${action}/${exampleId}.json`
-        const directory = path.split('/').slice(0, -1).join('/')
-        if (!fs.existsSync(directory)) {
-          fs.mkdirSync(directory, {
-            recursive: true,
-          })
-        }
-        fs.writeFileSync(path, JSON.stringify(message, null, 2))
+      const mockInstance = jest
+        .spyOn(
+          BaseAction.prototype as unknown as {
+            sendMessage: (key: string, message: DiscordMessage) => Promise<void>
+          },
+          'sendMessage'
+        )
+        .mockImplementationOnce((_: string, message: DiscordMessage) => {
+          const eventName = definition.name
+          const action = 'action' in example ? example.action : ''
+          const path = `data/debug/${eventName}/${action}/${exampleId}.json`
+          const directory = path.split('/').slice(0, -1).join('/')
+          if (!fs.existsSync(directory)) {
+            fs.mkdirSync(directory, {
+              recursive: true,
+            })
+          }
+          fs.writeFileSync(path, JSON.stringify(message, null, 2))
 
-        return Promise.resolve()
-      })
+          return Promise.resolve()
+        })
 
       const response = await app.inject({
         method: 'POST',
@@ -58,7 +68,9 @@ describe('Get embed', () => {
 
       mockInstance.mockRestore()
 
-      if(response.body.includes('An error occurred: Method not implemented.')) {
+      if (
+        response.body.includes('An error occurred: Method not implemented.')
+      ) {
         return
       }
       expect(response.statusCode).toBe(200)
