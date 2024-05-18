910e14b5a150a0d4956af03dd0b4c83a722e0012
Tomachi <8929706+book000@users.noreply.github.com>
2024-05-18 23:09:50 +0900

    feat: supported star trigger (#544)

--- src/actions/star.ts
+++ src/actions/star.ts
@@ -1,8 +1,23 @@
 import { StarEvent } from '@octokit/webhooks-types'
 import { BaseAction } from '.'
+import { createEmbed } from '../utils'
+import { EmbedColors } from '../embed-colors'
 
 export class StarAction extends BaseAction<StarEvent> {
   public run(): Promise<void> {
-    throw new Error('Method not implemented.')
+    const embed = createEmbed(this.eventName, EmbedColors.Star, {
+      title: `Starred ${this.event.repository.full_name} by ${this.event.sender.login}`,
+      url: this.event.repository.html_url,
+      author: {
+        name: this.event.sender.login,
+        icon_url: this.event.sender.avatar_url,
+        url: this.event.sender.html_url,
+      },
+    })
+
+    const key = `${this.event.repository.full_name}-star-${this.event.sender.login}`
+    return this.sendMessage(key, {
+      embeds: [embed],
+    })
   }
 }
