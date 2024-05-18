3332dd73f5f18f3ff4a63d4ee24daa8a0a4cb96b
Tomachi <8929706+book000@users.noreply.github.com>
2024-05-18 23:19:25 +0900

    feat: supported fork trigger (#545)

--- src/actions/fork.ts
+++ src/actions/fork.ts
@@ -1,8 +1,23 @@
 import { ForkEvent } from '@octokit/webhooks-types'
 import { BaseAction } from '.'
+import { createEmbed } from '../utils'
+import { EmbedColors } from '../embed-colors'
 
 export class ForkAction extends BaseAction<ForkEvent> {
   public run(): Promise<void> {
-    throw new Error('Method not implemented.')
+    const embed = createEmbed(this.eventName, EmbedColors.Fork, {
+      title: `Forked ${this.event.repository.full_name} by ${this.event.sender.login} to ${this.event.forkee.full_name}`,
+      url: this.event.forkee.html_url,
+      author: {
+        name: this.event.sender.login,
+        icon_url: this.event.sender.avatar_url,
+        url: this.event.sender.html_url,
+      },
+    })
+
+    const key = `${this.event.repository.full_name}-star-${this.event.sender.login}`
+    return this.sendMessage(key, {
+      embeds: [embed],
+    })
   }
 }
